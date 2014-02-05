febb86d1d82cac07d418e474827a270e961ca068
sattygalle <sattygalle@memory.ucsf.edu>
2014-02-05 14:55:40 -0800

    updates

--- longitudinalVBM/scripts/SA_run_group_SPM12LongRegPipe_generic.m
+++ longitudinalVBM/scripts/SA_run_group_SPM12LongRegPipe_generic.m
@@ -78,6 +78,43 @@ if jobstorun.DARTELregistration_to_existing == 1
     inputs{1,1} = rc1files; %rc1avg
     inputs{2,1} = rc2files; %rc2avg
     
+    matlabbatch{1}.spm.tools.dartel.warp1.images = {
+                                                '<UNDEFINED>'
+                                                '<UNDEFINED>'
+                                                }';
+matlabbatch{1}.spm.tools.dartel.warp1.settings.rform = 0;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).rparam = [4 2 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).template = {fullfile(dartelpath, 'Template_1.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).rparam = [2 1 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).K = 0;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).template = {fullfile(dartelpath, 'Template_2.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).rparam = [1 0.5 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).K = 1;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).template = {fullfile(dartelpath, 'Template_3.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).rparam = [0.5 0.25 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).K = 2;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).template = {fullfile(dartelpath, 'Template_4.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).K = 4;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).template = {fullfile(dartelpath, 'Template_5.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).its = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).rparam = [0.25 0.125 1e-06];
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).K = 6;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).template = {fullfile(dartelpath, 'Template_6.nii')};
+matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.lmreg = 0.01;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.cyc = 3;
+matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.its = 3;
+    
+    
+    
+    
+    
     spm('defaults', 'PET');
     spm_jobman('initcfg');
     spm_jobman('run', fullfile( jobspath, 'SPM12_dartel_existingtemplate_job.m'), inputs{:});
