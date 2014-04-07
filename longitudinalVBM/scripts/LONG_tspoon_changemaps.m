75ace73e8160f2ad493dd444a76b1a0930a5c1e2
sattygalle <sattygalle@memory.ucsf.edu>
2014-04-07 14:15:14 -0700

    add more generic smoothing function and begin tspoon function

---
+++ longitudinalVBM/scripts/LONG_tspoon_changemaps.m
@@ -0,0 +1,28 @@
+function scans_to_process = LONG_tspoon_changemaps( scans_to_process)
+%LONG_tspoon_changemaps - apply t-spoon smoothing to change maps
+%
+% Syntax:  scans_to_process = LONG_tspoon_changemaps( scans_to_process,scantype )
+%
+% Inputs:   scans_to_process - array of objects of class LONG_participant,
+%
+% Outputs: scans_to_process - updated array with run status
+%
+% Other m-files required: LONG_participant.m, LONG_setup.m, SPM12b
+% Subfunctions:
+%
+% MAT-files required: none
+%
+% See also:
+
+% To Do: 
+%
+% Author: Suneth Attygalle
+% Created 04/07/2014
+%
+% Revisions:
+
+
+    
+    
+end
+
