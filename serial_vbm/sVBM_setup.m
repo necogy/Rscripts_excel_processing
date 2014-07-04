31171bb20f7b9a76d63e158d4fe7f6d328e37a43
sattygalle <sattygalle@memory.ucsf.edu>
2014-07-03 17:36:57 -0700

    add steps and documentation to setup script

--- serial_vbm/sVBM_setup.m
+++ serial_vbm/sVBM_setup.m
@@ -1,28 +1,57 @@
 %% Serial VBM Processing Pipeline
-
+%
 % Preprocesses date using SPM12b and performs longitudinal registration
 % with multiple timepoints
-% Can be used to process single timepoint data. 
+% Can be used to process single timepoint data as well, but some options
+% might be tailored towards longitudinal processing.
+%
 % For two timepoint longitudinal data, use the longitudinalVBM package
 % instead.
- 
-% Set all of your file paths first in LONG_config.  Create a copy of this
-% file if you make edits to for a specific data set
-% set.
+%
+% To use this pippeline. Set all of your file paths first in sVBM_config.m 
+% Create a copy of the config file if you make edits to for a specific data set
+% set so it doesn't get overwritten if you download a new version of the
+% code in the same directory.
+%
+% Make sure to add spm12b folder (not with subfolders) and the sVBM folder with subfolders
+% to the Matlab path. Steps will error out if SPM12 is not added to the
+% path.
+%
+% for easier use of this script enable cell evaluation in matlab settings. 
+%
+% Suneth Attygalle - 7/1/14 - Memory and Aging Center
 
-%make sure to add spm12b folder and longitudinalVBM folder with subfolders
-%to matlab path.
 
-% enable cell evaluation in matlab settings for easier use of this script
+%% Initialize Things:
 clear
 clear classes
 
 %load parameters:
-sVBM_config % or name of new config file 
+sVBM_config % or name of edited config file 
 
-%scandatafolder = fullfile( SAreturnDriveMap('R'),'groups','rosen','longitudinalVBM','testfolder');
-%scandatafolder = fullfile( SAreturnDriveMap('R'),'groups','rosen','longitudinalVBM','FLOOR_Mar2014_reprocess_2','images','images_dir');
+%set scan data folder where image were placed using image_finder.sh
 scandatafolder = 'R:\groups\rosen\longitudinalVBM\gene_carriers_serialVBMtest';
+
+%read in directories and store info in scans_to_process structure
 scans_to_process = sVBM_load_rawdata( scandatafolder );
 
-scans_to_process2 = sVBM_run_segmentation(scans_to_process);
+%% Segmentation of timepoints:
+scans_to_process2 = sVBM_run_segmentation(scans_to_process); % (will segment all available timepoints in the directories)
+
+%% Longitudinal Registration
+
+%% Segmentation of Longitudinal Images
+
+%% Multiply Change Maps with Segmentations
+
+%% Transform Longitudinal Images to Group/MNI space
+
+%% Transform Timepoint Data to MNI using Intermediate Longitudinal Image Warp
+
+%% Warp ROIs from Atlas Space to Native Timepoint Space.
+
+
+
+
+
+
