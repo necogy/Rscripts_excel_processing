a3c287f6f0a4cf78f352776b44292928cb622149
sattygalle <sattygalle@memory.ucsf.edu>
2014-10-24 17:14:35 -0700

    tissue volume job

---
+++ longitudinalVBM/scratchfolder/tissuevolumescript_job.m
@@ -0,0 +1,9 @@
+%-----------------------------------------------------------------------
+% Job saved on 16-Oct-2014 10:57:48 by cfg_util (rev $Rev: 6134 $)
+% spm SPM - SPM12 (6225)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.util.tvol.matfiles = '<UNDEFINED>';
+matlabbatch{1}.spm.util.tvol.tmax = 3;
+matlabbatch{1}.spm.util.tvol.mask = {'R:\groups\imaging_core\software\spm12\tpm\mask_ICV.nii,1'};
+matlabbatch{1}.spm.util.tvol.outf = '';
