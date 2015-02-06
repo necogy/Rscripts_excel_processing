11445588dbe298e71767b649794938b103820752
sattygalle <sattygalle@memory.ucsf.edu>
2015-02-06 14:27:57 -0800

    fix LONG_participant to find only MP-LAS and ignore MP-LAPMP_LASlong by changing regexp

--- longitudinalVBM/scripts/LONG_setup.m
+++ longitudinalVBM/scripts/LONG_setup.m
@@ -22,6 +22,8 @@ LONG_config %<-------------- OPEN THIS and replace with paths specific to your s
 % b. Load Data
 scans_to_process = LONG_load_inputfile( scandatafolder );
 
+%%PREPROCESSING
+
 %% 2. Longitudinal registration to generate subject average images 
 scans_to_process = LONG_run_registration( scans_to_process ); 
 
@@ -34,7 +36,7 @@ scans_to_process = LONG_run_segmentation( scans_to_process, 'mean', spmpath );
 %voxelsize =1;
 %scans_to_process = LONG_DARTELimport( scans_to_process, voxelsize ); 
 
-%% 4. inter-subject registration of mean images using Dartel (requires template
+%% 4. Inter-subject registration of mean images using Dartel (requires template)
 % or create a new one
 
 % specify subjects to include to generate a new template(subset of all available patients in your
@@ -54,15 +56,14 @@ scans_to_process = LONG_multiply_segments_with_change(scans_to_process); %this w
 %% 6. Transform longitudinal images to group/MNI space
 scans_to_process = LONG_DARTEL_to_MNI(scans_to_process, templatepath);
 
-%% Segment time1 and time2 images:
-scans_to_process = LONG_run_segmentation( scans_to_process, 'time1', spmpath ); 
-scans_to_process = LONG_run_segmentation( scans_to_process, 'time2', spmpath ); 
+
+
 
 %% Group results:
 
 %% generate average maps of c_jd/dv maps and wholebrain 
 LSD_PIDNs = [98;588;951;1004;1176;1319;1340;1463;1586;2275;2500;2711;3521;4160;4375;4379;4471;5468;5830;6110;10114;10880;11735;11965;12555;13108;13138;13185;13272;13512;13919;14427;15774];
-RSD_PIDNs = [84;278;1615;2522;3690;3824;4747;6600;9283;10032;10434;11028;11704;11773;13962];
+RSD_PIDNs = [84;278;1615;2522;3690;3824;4747;6600;9283;10032;10434;11028;11704;11773;13962];  
 SD_PIDNs = [LSD_PIDNs; RSD_PIDNs];
 HC_PIDNs = [841;1124;1362;1416;1418;1813;2046;2062;2557;2679;2680;2688;2692;2699;2702;2703;2715;2720;2732;2735;2743;2744;2774;2801;3015;3027;3530;3773;4062;4063;4348;4943;5061;5064;5436;5595;5627;5844;65;1641;3700;3884;5133;5888;6248;6741;6838;6842;6857;6860;6867;6868;6908;6909;6922;6934;6935;6976;6977;7142;7396;7397;7411;7418;7444;7749;7792;7793;7802;7811;7813;7837;7838;7850;7851;7938;8182;8193;8510;8533;8538;8545;8565;8590;8592;8593;8594;8601;8619;8627;8698;8706;8913;9320;9440;9621;9757;10445;10683;11241;11247;11296;11329;11463;11727];
 %scans_to_process = LONG_generatemeanmaps(scans_to_process, PIDNlistforgroup, 'titletoprependtoaverageimage');
@@ -71,6 +72,9 @@ scans_to_process = LONG_generatemeanmaps(scans_to_process, LSD_PIDNs, 'SDL');
 scans_to_process = LONG_generatemeanmaps(scans_to_process, RSD_PIDNs, 'SDR');
 scans_to_process = LONG_generatemeanmaps(scans_to_process, SD_PIDNs, 'SDLR');
 scans_to_process = LONG_generatemeanmaps(scans_to_process, HC_PIDNs, 'HC');
+%% Segment time1 and time2 images:
+scans_to_process = LONG_run_segmentation( scans_to_process, 'time1', spmpath ); 
+scans_to_process = LONG_run_segmentation( scans_to_process, 'time2', spmpath ); 
 
 %% Transform time1 and time2 data to mni using intermediate longitudinal image warp
 % This step might be broken.
