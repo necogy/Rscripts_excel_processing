a9e4fcd6c5a6ffabed0906c6a7f76aede057aef6
sattygalle <sattygalle@memory.ucsf.edu>
2015-02-06 16:09:00 -0800

    update LONG_generatemeanmaps so it can run on all subjects and combine sections for wmgm and just gmi extractions in long_setup

--- longitudinalVBM/scripts/LONG_setup.m
+++ longitudinalVBM/scripts/LONG_setup.m
@@ -22,7 +22,7 @@ LONG_config %<-------------- OPEN THIS and replace with paths specific to your s
 % b. Load Data
 scans_to_process = LONG_load_inputfile( scandatafolder );
 
-%%PREPROCESSING
+%%PREPROCESSING (Steps 2-6)
 
 %% 2. Longitudinal registration to generate subject average images 
 scans_to_process = LONG_run_registration( scans_to_process ); 
@@ -55,56 +55,51 @@ scans_to_process = LONG_multiply_segments_with_change(scans_to_process); %this w
 
 %% 6. Transform longitudinal images to group/MNI space
 scans_to_process = LONG_DARTEL_to_MNI(scans_to_process, templatepath);
+% at this point you have generated warped change maps that can be used for
+% statistical analysis, you might want to smooth the images using the
+% following steps.
+
+%% Smooth individual participant change maps images for stats 
+fwhm = 6 ; % <-- this should be changed depending on the data
+scans_to_process = LONG_smooth_changemaps(scans_to_process, fwhm);
+
+%% T-Spoon for stats ( you might not want to use this vs normal smoothing below)
+% T-spoon smoothing is an improved algorithm for smoothing that reduces the
+% effect of smoothing the brain outside of the actual brain
+scans_to_process = LONG_tspoon_changemaps(scans_to_process);
 
 
+%% extract mean/median change values in ROIs and save to scans_to_process
+% paths to ROI folder should be set in LONG_config
+ROIprefix = '^s'; % <-- the ROIs that will be used to to extract will start with the prefix here
+changemapprefix = 'wl_c1avg_jd_'; % <---Specify  the image you want to extract from by including the prefix of the file here
+scans_to_process = LONG_extractROIs(scans_to_process, changemapprefix, pathtoROIs, ROIprefix); %extract GM ROI values and add to scans_to_process structure
+scans_to_process = LONG_extractWMGMROIs(scans_to_process, changemapprefix, pathtoROIs, ROIprefix); % if you want combined GM/WM extractions, run this line instead.
 
+[ROImeans, ROImedians] = LONG_exportROIs(scans_to_process); %pull out mean and median values from scans_to_process in a convenient format
 
-%% Group results:
+%% optional steps
 
-%% generate average maps of c_jd/dv maps and wholebrain 
-LSD_PIDNs = [98;588;951;1004;1176;1319;1340;1463;1586;2275;2500;2711;3521;4160;4375;4379;4471;5468;5830;6110;10114;10880;11735;11965;12555;13108;13138;13185;13272;13512;13919;14427;15774];
-RSD_PIDNs = [84;278;1615;2522;3690;3824;4747;6600;9283;10032;10434;11028;11704;11773;13962];  
-SD_PIDNs = [LSD_PIDNs; RSD_PIDNs];
-HC_PIDNs = [841;1124;1362;1416;1418;1813;2046;2062;2557;2679;2680;2688;2692;2699;2702;2703;2715;2720;2732;2735;2743;2744;2774;2801;3015;3027;3530;3773;4062;4063;4348;4943;5061;5064;5436;5595;5627;5844;65;1641;3700;3884;5133;5888;6248;6741;6838;6842;6857;6860;6867;6868;6908;6909;6922;6934;6935;6976;6977;7142;7396;7397;7411;7418;7444;7749;7792;7793;7802;7811;7813;7837;7838;7850;7851;7938;8182;8193;8510;8533;8538;8545;8565;8590;8592;8593;8594;8601;8619;8627;8698;8706;8913;9320;9440;9621;9757;10445;10683;11241;11247;11296;11329;11463;11727];
+%% Generate average maps of warped c_jd/dv maps and wholebrain for plotting
+% you can specify the PIDNs so you can plot averages of subsets of subjects
+% (e.g by group/disease) use PIDNlist = []; to use all subjects
+PIDNsforAverage = [98;588;951;1004;1176;1319;1340;1463;1586;2275;2500;2711;3521;4160;4375;4379;4471;5468;5830;6110;10114;10880;11735;11965;12555;13108;13138;13185;13272;13512;13919;14427;15774];
 %scans_to_process = LONG_generatemeanmaps(scans_to_process, PIDNlistforgroup, 'titletoprependtoaverageimage');
+scans_to_process = LONG_generatemeanmaps(scans_to_process,PIDNsforAverage, 'titleForImage');
 
-scans_to_process = LONG_generatemeanmaps(scans_to_process, LSD_PIDNs, 'SDL');
-scans_to_process = LONG_generatemeanmaps(scans_to_process, RSD_PIDNs, 'SDR');
-scans_to_process = LONG_generatemeanmaps(scans_to_process, SD_PIDNs, 'SDLR');
-scans_to_process = LONG_generatemeanmaps(scans_to_process, HC_PIDNs, 'HC');
 %% Segment time1 and time2 images:
 scans_to_process = LONG_run_segmentation( scans_to_process, 'time1', spmpath ); 
 scans_to_process = LONG_run_segmentation( scans_to_process, 'time2', spmpath ); 
+
+%% %%%%%%% USE STEPS BELOW AT YOUR OWN RISK, not all fully vetted %%%%%%%%%%%%%
 
 %% Transform time1 and time2 data to mni using intermediate longitudinal image warp
 % This step might be broken.
 scans_to_process = LONG_timepoint_to_MNI(scans_to_process, templatepath, 'time1');
 scans_to_process = LONG_timepoint_to_MNI(scans_to_process, templatepath, 'time2');
 
-%% T-Spoon for stats ( you might not want to use this vs normal smoothing below)
-scans_to_process = LONG_tspoon_changemaps(scans_to_process);
-
-%% Smooth individual participant change maps images for stats 
-fwhm = 6 ;
-scans_to_process = LONG_smooth_changemaps(scans_to_process, fwhm);
-
-%% extract mean/median change values in ROIs and save to scans_to_process
-%structure
-%pathtoROIs = fullfile( SAreturnDriveMap('R'),'groups','rosen','longitudinalVBM','ROIs');
-ROIprefix = '^s';
-changemapprefix = 'wl_c1avg_jd_';
-scans_to_process = LONG_extractROIs(scans_to_process, changemapprefix, pathtoROIs, ROIprefix); %extract ROI values and add to scans_to_process structure
-[ROImeans, ROImedians] = LONG_exportROIs(scans_to_process); %pull out mean and median values from scans_to_process in a convenient format
-
-%% extract mean/median changes values from combined GM/WM change maps
-ROIprefix = '^rr';
-changemapprefix = 'wl_c*avg_jd_';
-scans_to_process = LONG_extractWMGMROIs(scans_to_process, changemapprefix, pathtoROIs, ROIprefix)
-[ROImeans, ROImedians] = LONG_exportROIs(scans_to_process); %pull out mean and median values from scans_to_process in a convenient format
-
 %% Extract ROIs from change maps in Dartel Space
 scans_to_process = LONG_extractROIsinDARTEL(scans_to_process, changemapprefix, pathtoROIs, ROIprefix); 
-
 
 %% extract mean/median change values from warped timepoints (MNI) and save to scans_to_process
 %pathtoROIs = fullfile( SAreturnDriveMap('R'),'groups','rosen','longitudinalVBM','ROIs');
@@ -117,7 +112,6 @@ nativeROIvolumes_time2 = LONG_exportMNI_ROIs(scans_to_process, 'time2');
 %% extract time 1 and time 2 volumes
 scans_to_process = LONG_extractVolumes(scans_to_process, 'time1'); 
 scans_to_process = LONG_extractVolumes(scans_to_process, 'time2'); 
-
 
 %% DTI related scripts below %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
