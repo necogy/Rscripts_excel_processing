773d3cf7ae863dc97a49b068007d1f33ce37091e
Yann Cobigo <yann.cobigo@yahoo.com>
2015-03-05 17:28:54 -0800

    Implementation of signal-to-noise ratio for 3D images

--- new_arch/Quality_control/Signal_to_noise_ratio.py
+++ new_arch/Quality_control/Signal_to_noise_ratio.py
@@ -1,81 +1,143 @@
+import logging
 import numpy
 import Quality_control
+
+_log = logging.getLogger("__Signal_to_noise_ratio__")
 
 class Signal_to_noise_ratio( Quality_control.Quality_control ):
     """Signal-to-noise ratio (SNR) calculation.
     
     Attributes:
-    image_:Super:NiftiImage   - image data
-    setup_:map                - mapping of the setup
+    Rayleigh_:double   - Rayleigh threashold. If the ration of (Mean/Sd)_{background} > Rayleigh_, the background does not follow a Rayleigh distribution and we can't use:
+
+    SNR_{sd} = \fract{\mu_{signal}}{1.52 * \sigma_{background}}
+
     """
+    #
+    #
     def __init__( self, Image_name ):
         """Return a new Signal to noise ratio instance."""
         super( Signal_to_noise_ratio, self ).__init__( Image_name )
-
-    def average_signal_( self ):
-        average = 0
-        count   = 0
-        # for index = (i,j,k), value in numpy.ndenumerate( self.brain_.data ):
-        for index, value in numpy.ndenumerate( self.brain_.data ):
-            if self.brain_mask_.data[index] == 1:
-                average += self.brain_.data[index]
-                count   += 1
-        #
-        if count != 0:
-            average /= count
-            print average
-        else:
-            print "Problem with the count"
-
-    def roi_selection_( self ):
-        # sform
-        #     [ 0.,  0.,  1.]
-        # R = [ 1.,  0.,  0.]
-        #     [ 0.,  1.,  0.]
-        #
-        # Please notice the order in which the dimensions are specified: (t, z, y, x).
-        # (i,j,k) = (1,2,3) => R(i,j,k) = (k,i,j) = (3,1,2)
-        (X, Y, Z)                = abs(self.image_.header['sform'][0:3,0:3]).round().dot((1,2,3))
-        (X_data, Y_data, Z_data) = abs(self.image_.header['sform'][0:3,0:3]).round().dot((3,2,1))
         
-        #
-        # Regions of interest
-        #
-        box_size = 10
-        # ROI selection
-        roi_selection = [
-            {
-                X_data: slice( 0, box_size ),
-                Y_data: slice( 0, box_size ),
-                Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
-            },
-            #
-            {
-                X_data: slice( self.image_.header['dim'][int(X)] - box_size, self.image_.header['dim'][int(X)] ),
-                Y_data: slice( 0, box_size ),
-                Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
-            },
-            #
-            {
-                X_data: slice( 0, box_size ),
-                Y_data: slice( self.image_.header['dim'][int(Y)] - box_size, self.image_.header['dim'][int(Y)] ),
-                Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
-            },
-            #
-            {
-                X_data: slice( self.image_.header['dim'][int(X)] - box_size, self.image_.header['dim'][int(X)] ),
-                Y_data: slice( self.image_.header['dim'][int(Y)] - box_size, self.image_.header['dim'][int(Y)] ),
-                Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
-            } ]
-        #
-        # ROI
-        #self.image_.data[ roi_selection[0][1], roi_selection[0][2], roi_selection[0][3]] = 1000
-        #self.image_.data[ roi_selection[1][1], roi_selection[1][2], roi_selection[1][3]] = 1000
-        #self.image_.data[ roi_selection[2][1], roi_selection[2][2], roi_selection[2][3]] = 1000
-        #self.image_.data[ roi_selection[3][1], roi_selection[3][2], roi_selection[3][3]] = 1000
+        self.Rayleigh_         = 1.91
+        self.mean_signal_      = 0.
+        self.mean_background_  = 0.
+        self.sigma_background_ = 0.
 
-        average = self.image_.data[ roi_selection[0][1], roi_selection[0][2], roi_selection[0][3]].mean()
-        std =  self.image_.data[ roi_selection[0][1], roi_selection[0][2], roi_selection[0][3]].std()
-        print average
-        print std
 
+    #
+    #
+    def average_signal_( self ):
+        """Average signal caculates the mean signal over the brain area."""
+        try:
+            count   = 0
+            # for index = (i,j,k), value in numpy.ndenumerate( self.brain_.data ):
+            for index, value in numpy.ndenumerate( self.brain_.data ):
+                if self.brain_mask_.data[index] == 1:
+                    self.mean_signal_ += self.brain_.data[index]
+                    count   += 1
+            #
+            if count != 0:
+                self.mean_signal_ /= count
+            else:
+                raise Exception( "Problem with the count of voxels.")
+
+        except Exception as inst:
+            print inst
+            _log.error(inst)
+            quit(-1)
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            quit(-1)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            quit(-1)
+
+
+    #
+    #
+    def roi_selection_( self ):
+        """Region of interest function calculates the background signal within four regions and estimates the standard deviation of the signal in those region."""
+        try:
+            # sform
+            #     [ 0.,  0.,  1.]
+            # R = [ 1.,  0.,  0.]
+            #     [ 0.,  1.,  0.]
+            #
+            # Please notice the order in which the dimensions are specified: (t, z, y, x).
+            # (i,j,k) = (1,2,3) => R(i,j,k) = (k,i,j) = (3,1,2)
+            (X, Y, Z)                = abs(self.image_.header['sform'][0:3,0:3]).round().dot((1,2,3))
+            (X_data, Y_data, Z_data) = abs(self.image_.header['sform'][0:3,0:3]).round().dot((3,2,1))
+        
+            #
+            # Regions of interest
+            #
+            box_size = 10
+            # ROI selection
+            roi_selection = [
+                {
+                    X_data: slice( 0, box_size ),
+                    Y_data: slice( 0, box_size ),
+                    Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
+                },
+                #
+                {
+                    X_data: slice( self.image_.header['dim'][int(X)] - box_size, self.image_.header['dim'][int(X)] ),
+                    Y_data: slice( 0, box_size ),
+                    Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
+                },
+                #
+                {
+                    X_data: slice( 0, box_size ),
+                    Y_data: slice( self.image_.header['dim'][int(Y)] - box_size, self.image_.header['dim'][int(Y)] ),
+                    Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
+                },
+                #
+                {
+                    X_data: slice( self.image_.header['dim'][int(X)] - box_size, self.image_.header['dim'][int(X)] ),
+                    Y_data: slice( self.image_.header['dim'][int(Y)] - box_size, self.image_.header['dim'][int(Y)] ),
+                    Z_data: slice( self.image_.header['dim'][int(Z)] - 20, self.image_.header['dim'][int(Z)] )
+                } ]
+            #
+            # ROI
+            #self.image_.data[ roi_selection[0][1], roi_selection[0][2], roi_selection[0][3]] = 1000
+            #self.image_.data[ roi_selection[1][1], roi_selection[1][2], roi_selection[1][3]] = 1000
+            #self.image_.data[ roi_selection[2][1], roi_selection[2][2], roi_selection[2][3]] = 1000
+            #self.image_.data[ roi_selection[3][1], roi_selection[3][2], roi_selection[3][3]] = 1000
+
+            self.mean_background_ = self.image_.data[ roi_selection[0][1], roi_selection[0][2], roi_selection[0][3]].mean()
+            self.sigma_background_ =  self.image_.data[ roi_selection[0][1], roi_selection[0][2], roi_selection[0][3]].std()
+
+        except Exception as inst:
+            print inst
+            _log.error(inst)
+            quit(-1)
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            quit(-1)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            quit(-1)
+
+
+    #
+    #
+    def process( self ):
+        """Process fuction calculates the signal-to-noise ratio."""
+        try:
+            if self.mean_background_ / self.sigma_background_ < self.Rayleigh_:
+                return self.mean_signal_  / (1.52 * self.sigma_background_ )
+            else:
+                 _log.warning("The background should not be calculated following Rayleigh's law.")
+                 return -1
+            
+        except Exception as inst:
+            print inst
+            _log.error(inst)
+            quit(-1)
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            quit(-1)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            quit(-1)
--- new_arch/pipeline_QC.py
+++ new_arch/pipeline_QC.py
@@ -11,13 +11,14 @@ logging.basicConfig(filename='Quality_control.log',level=logging.DEBUG)
 #fullpath = os.path.join(os.path.sep,'mnt','macdata','groups', 'imaging_core', 'yann',
 #                        'PPG0246-1_Shearer,Robert','PVE_Segmentation','IM-0001-0001.nii')
 #fullpath = os.path.join("/mnt/macdata/groups/imaging_core/yann/PPG0246-1_Shearer,Robert/PVE_Segmentation/IM-0001-0001.nii")
-#fullpath = os.path.join("/mnt/macdata/groups/rosen/gabe/yann/new batch of images/MP-LAS_NIFD190X1_500.nii")
-fullpath = os.path.join("/mnt/macdata/groups/imaging_core/yann/PPG0246-1_Shearer,Robert/ACPC_Alignment/T2_PPG0246X1.nii")
+fullpath = os.path.join("/home/ycobigo/Images_test/MP-LAS_NIFD040X2_500.nii")
+#fullpath = os.path.join("/mnt/macdata/groups/imaging_core/yann/PPG0246-1_Shearer,Robert/ACPC_Alignment/T2_PPG0246X1.nii")
 #'PPG0246-1' for test;
 
 snr = SNR.Signal_to_noise_ratio( fullpath )
 snr.roi_selection_()
 snr.average_signal_()
+print snr.process()
 
 #asl = Arterial_Spin_Labeling.Protocol()
 #asl.patient_dir_ = os.path.join(fullpath, "PPG0246-1_Shearer,Robert")
